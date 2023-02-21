cc4b5695cc0565716f852a2f6a23539f117c5263
Taner Aytaş <120183459+taneraytas@users.noreply.github.com>
2023-02-22 01:19:36 +0300

    header linklere hover eklendi

--- src/index.js
+++ src/index.js
@@ -1,9 +1,34 @@
-import './less/index.less'
+import "./less/index.less";
 
 // Örnek bir event kullanımı aşağıdadır. Çalıştırmak için comment dışına alın
 // document.querySelector("h1").addEventListener("click",function(e){
 // alert("Bana tıkladın!")
 // });
 
-
 // Kodlar buraya gelecek!
+
+// let headerSec = document.querySelector("header");
+
+// headerSec.addEventListener("mouseover", (e) => {
+//   e.target.style.backgroundColor = "red";
+//   e.target.style.color = "white";
+// });
+// headerSec.addEventListener("mouseout", (e) => {
+//   e.target.style.backgroundColor = "";
+//   e.target.style.color = "white";
+// });
+
+const navItems = document.querySelectorAll(".nav-link");
+
+navItems.forEach((item) => {
+  item.addEventListener(
+    "mouseover",
+    (event) => {
+      event.target.style.color = "rgb(226,164,80)";
+      setTimeout(() => {
+        event.target.style.color = "";
+      }, 750);
+    },
+    false
+  );
+});
